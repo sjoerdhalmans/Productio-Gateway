68a31b76958dfd1878c95144f465f1a464ccde9a
SKULLCRUSHER957 <sjoerd.halmans@gmail.com>
2021-03-29 16:20:01 +0200

    First connection with frontend fixed. Permissions are being read

--- index.js
+++ index.js
@@ -1,17 +1,48 @@
 var express = require('express');
 var app = express();
-var router = require('./routers/router')
+var jwt = require('express-jwt');
+var jwks = require('jwks-rsa');
+const router = require('./routers/feedService');
 var bodyParser = require('body-parser');
-
+var jwtAuthz = require('express-jwt-authz');
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }));
+var cors = require('cors')
+
+var port = 3000
+app.use(cors());
+
+var jwtCheck = jwt({
+      secret: jwks.expressJwtSecret({
+          cache: true,
+          rateLimit: true,
+          jwksRequestsPerMinute: 5,
+          jwksUri: 'https://deathrun.auth0.com/.well-known/jwks.json'
+    }),
+    audience: 'https://productiogateway',
+    issuer: 'https://deathrun.auth0.com/',
+    algorithms: ['RS256']
+});
+
+var options = {
+  customScopeKey: 'permissions'
+};
+
+app.use(jwtCheck);
+app.use(router);
 
 app.get('/', (req, res) => {
     res.send("Simple API Gateway")
 })
 
-app.use(router)
+router.get('/tests',
+  jwtAuthz(['read:feed'], options),
+  function (req, res) { 
+    console.log(req)
+    res.send('well') });
 
-console.log("Simple API Gateway run on localhost:3000")
+console.log('gateway is operational at ' + port)
 
-app.listen(3000);
+app.listen(port);
+
+module.exports = { jwt }
--- routers/feedService.js
+++ routers/feedService.js
@@ -1,14 +1,18 @@
 var express = require('express');
 var router = express.Router()
 const apiAdapter = require('./apiAdapter')
+var { jwt } = require('../index');
+var jwtAuthz = require('express-jwt-authz');
 
 const BASE_URL = 'http://localhost:8000'
 const api = apiAdapter(BASE_URL)
 
 router.get('/feeds', (req, res) => {
+  console.log(req)
   api.get(req.path).then(resp => {
     res.send(resp.data)
   })
+  res.send('well')
 })
 
 router.get('/feeds/:hashtag', (req, res) => {
