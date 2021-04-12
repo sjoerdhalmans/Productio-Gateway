30250f05392b404db75664653f22ea9763e0f361
SKULLCRUSHER957 <sjoerd.halmans@gmail.com>
2021-04-12 22:23:45 +0200

    Added logistics service

---
+++ routers/logisticsService.js
@@ -0,0 +1,56 @@
+const { default: axios } = require('axios');
+var express = require('express');
+var router = express.Router()
+var jwtAuthz = require('express-jwt-authz');
+const redis = require("redis");
+
+var options = {
+    customScopeKey: 'permissions'
+};
+
+// Configures redis client
+let
+    /* Values are hard-coded for this example, it's usually best to bring these in via file or environment variable for production */
+    client = redis.createClient({
+        port: 6379,               // replace with your port
+        host: '127.0.0.1',        // replace with your hostanme or IP address
+    });
+
+router.get('/getreceipts', jwtAuthz(['read:feed'], options), async (req, res) => {
+    var response
+
+    await axios.get('http://localhost:8086/api/getAllReceipts').then(res => {
+        response = res.data;
+    })
+
+    res.send(response);
+})
+
+router.get('/getorders', jwtAuthz(['read:feed'], options), async (req, res) => {
+    var response
+
+    await axios.get('http://localhost:8086/api/getAllOrders').then(res => {
+        response = res.data;
+    })
+
+    res.send(response);
+})
+
+router.post('/createorder', jwtAuthz(['read:feed'], options), (req, res) => {
+    console.log('test')
+    res.send(client.publish('createOrder', JSON.stringify(req.body), redis.print));
+})
+
+router.post('/archiveorder', jwtAuthz(['read:feed'], options), (req, res) => {
+    res.send(client.publish('archiveOrder', JSON.stringify(req.body), redis.print));
+})
+
+router.put('/updateorder', jwtAuthz(['read:feed'], options), (req, res) => {
+    res.send(client.publish('updateOrder', JSON.stringify(req.body), redis.print));
+})
+
+router.delete('/deleteorder', jwtAuthz(['read:feed'], options), (req, res) => {
+    res.send(client.publish('deleteOrder', JSON.stringify(req.body), redis.print));
+})
+
+module.exports = router
--- routers/router.js
+++ routers/router.js
@@ -4,7 +4,7 @@ var hashtagService = require('./hashtagService')
 var feedService = require('./feedService')
 var pingService = require('./pingService')
 var hrService = require('./hrService')
-
+var logisticsService = require('./logisticsService')
 router.use((req, res, next) => {
     console.log("Called: ", req.path)
     next()
@@ -14,5 +14,6 @@ router.use(feedService)
 router.use(hashtagService)
 router.use(pingService)
 router.use(hrService)
+router.use(logisticsService)
 
 module.exports = router
